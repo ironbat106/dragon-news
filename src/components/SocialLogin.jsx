79a69ba0999b6a279ddd859c248c23456814e67f
ironbat106 <aryanahadateeq02@gmail.com>
2024-11-17 18:28:58 +0600

    finalizing

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -5,12 +5,15 @@ const SocialLogin = () => {
         <div>
             <h2 className="font-semibold mb-3">Login with</h2>
             <div className="*:w-full flex flex-col space-y-2">
+
                 <button className="btn text-blue-600">
                     <FaGoogle></FaGoogle> Login with Google
                 </button>
+
                 <button className="btn">
                     <FaGithub></FaGithub> Login with Github
                 </button>
+                
             </div>
         </div>
     );
